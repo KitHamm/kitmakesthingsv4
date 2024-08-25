ca8f25d498c03f06c5795de22a6bcc79c6903e03
Kit Hamm <kitmakesthings@gmail.com>
2024-08-25 15:52:27 +0100

    -fixed page width on privacy

--- src/app/(main)/privacy/page.tsx
+++ src/app/(main)/privacy/page.tsx
@@ -3,7 +3,7 @@ import Privacy from "@/components/Privacy";
 export default function PrivacyPage() {
     return (
         <main>
-            <section className="top-0 left-0 min-w-[100dvw] xl:py-40 py-20">
+            <section className="top-0 left-0 overflow-x-hidden xl:py-40 py-20">
                 <Privacy />
             </section>
         </main>
