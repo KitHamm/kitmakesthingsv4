c6af6c0743c7b7d59323651743a1cab18d6971e7
Kit Hamm <kitmakesthings@gmail.com>
2024-10-17 12:54:41 +0100

    -fixed ravalidate landing

--- src/components/actions/ContentActions.tsx
+++ src/components/actions/ContentActions.tsx
@@ -78,6 +78,7 @@ export async function UpdateLanding(data: LandingFormType) {
             return Promise.resolve({ status: 201, message: err });
         } finally {
             revalidatePath("/dashboard/content");
+            revalidatePath("/");
         }
     } catch (err: any) {
         return Promise.resolve({ status: 201, message: err });
