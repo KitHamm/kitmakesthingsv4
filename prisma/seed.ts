32950aeaae32063464a59bd700c9f64bbf942f10
Kit Hamm <kitmakesthings@gmail.com>
2025-01-17 20:27:00 +0000

    -fixed file upload

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,40 +4,40 @@ import { Role } from "@prisma/client";
 import { hash } from "bcrypt";
 
 async function main() {
-    const oldInvoiceItems = await prisma.invoiceItem.findMany();
+	const oldInvoiceItems = await prisma.invoiceItem.findMany();
 
-    // for (let i = 0; i < oldInvoiceItems.length; i++) {
-    //     await prisma.invoiceItem.update({
-    //         where: {
-    //             id: oldInvoiceItems[i].id,
-    //         },
-    //         data: {
-    //             invoiceReference: oldInvoiceItems[i].invocieReference,
-    //         },
-    //     });
-    // }
+	// for (let i = 0; i < oldInvoiceItems.length; i++) {
+	//     await prisma.invoiceItem.update({
+	//         where: {
+	//             id: oldInvoiceItems[i].id,
+	//         },
+	//         data: {
+	//             invoiceReference: oldInvoiceItems[i].invocieReference,
+	//         },
+	//     });
+	// }
 
-    // const hashedPassword = await hash("n7nNWFTkoejrs:Q3Phf", 12);
-    // await prisma.user.upsert({
-    //     where: {
-    //         email: "kitmakesthings@gmail.com",
-    //     },
-    //     update: {},
-    //     create: {
-    //         email: "kitmakesthings@gmail.com",
-    //         firstName: "Kit",
-    //         lastName: "Hamm",
-    //         password: hashedPassword,
-    //         role: Role.ADMIN,
-    //     },
-    // });
+	const hashedPassword = await hash("n7nNWFTkoejrs:Q3Phf", 12);
+	await prisma.user.upsert({
+		where: {
+			email: "kitmakesthings@gmail.com",
+		},
+		update: {},
+		create: {
+			email: "kitmakesthings@gmail.com",
+			firstName: "Kit",
+			lastName: "Hamm",
+			password: hashedPassword,
+			role: Role.ADMIN,
+		},
+	});
 }
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
--- src/app/api/upload/route.tsx
+++ src/app/api/upload/route.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import path from "path";
 import { writeFile } from "fs/promises";
 import sharp from "sharp";
+import { revalidatePath } from "next/cache";
 
 interface ArrayFile extends File {
 	arrayBuffer: () => Promise<ArrayBuffer>;
@@ -21,10 +22,9 @@ export async function POST(request: Request) {
 	const preBuffer = Buffer.from(await file.arrayBuffer());
 	const buffer = await sharp(preBuffer).webp().toBuffer();
 	const fileName = file.name.split(".")[0].replace(" ", "-");
-	const extension = file.name.split(".")[1];
 	const formattedDate = date.toISOString().replace(/:|\./g, "-");
 	const formattedName =
-		fileName.replace(" ", "_") + "-" + formattedDate + "." + extension;
+		fileName.replace(" ", "_") + "-" + formattedDate + ".webp";
 	try {
 		await writeFile(
 			path.join(
@@ -33,22 +33,17 @@ export async function POST(request: Request) {
 			),
 			buffer
 		);
-		try {
-			await prisma.images.create({
-				data: {
-					url: formattedName,
-				},
-			});
-			return new NextResponse(
-				JSON.stringify({ message: formattedName }),
-				{ status: 201 }
-			);
-		} catch {
-			return new NextResponse(
-				JSON.stringify({ error: "An Error Occurred" }),
-				{ status: 500 }
-			);
-		}
+
+		await prisma.images.create({
+			data: {
+				url: formattedName,
+			},
+		});
+		revalidatePath("dashboard/content");
+		revalidatePath("dashboard/media");
+		return new NextResponse(JSON.stringify({ message: formattedName }), {
+			status: 201,
+		});
 	} catch (error) {
 		return new NextResponse(
 			JSON.stringify({ error: "An Error Occurred" }),
