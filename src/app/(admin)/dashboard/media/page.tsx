a1737d7008a8db3d7c7d7b93673f08afaebb2e50
Kit Hamm <kitmakesthings@gmail.com>
2025-01-18 23:25:00 +0000

    -try add revalidate to media dashboard path

--- src/app/(admin)/dashboard/media/page.tsx
+++ src/app/(admin)/dashboard/media/page.tsx
@@ -1,6 +1,8 @@
 import MediaMain from "@/components/admin/media/MediaMain";
 import prisma from "@/lib/prisma";
 
+export const revalidate = 0;
+
 export default async function Media() {
 	const images = await prisma.images.findMany();
 	return (
